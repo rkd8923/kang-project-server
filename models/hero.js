3a4a6d8bf9207ea627416a7adcc7a87e7d3bf149
omnis-ubuntu <rkd8923@gmail.com>
2019-08-13 15:41:15 +0900

    make simple api server

---
+++ models/hero.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+
+var heroSchema = mongoose.Schema({
+  id: {
+    type: Number,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+});
+
+var Hero = mongoose.model('hero', heroSchema);
+module.exports = Hero;
---
+++ server.js
@@ -0,0 +1,35 @@
+var express    = require('express');
+var app        = express();
+var path       = require('path');
+var mongoose   = require('mongoose');
+var bodyParser = require('body-parser');
+
+// Database
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb+srv://rkd8923:1q2w3e4r@cluster0-8fxtu.mongodb.net/test?retryWrites=true&w=majority');
+var db = mongoose.connection;
+db.once('open', function () {
+   console.log('DB connected!');
+});
+db.on('error', function (err) {
+  console.log('DB ERROR:', err);
+});
+
+// Middlewares
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(function (req, res, next) { //1
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'content-type');
+  next();
+});
+
+// API
+app.use('/api/heroes', require('./api/heroes'));
+
+// Server
+var port = 3000;
+app.listen(port, function(){
+  console.log('listening on port:' + port);
+});
